e3bf5c74547608fb08c84d31ff8b6b11e6838d55
Koby Moon <moonkoby1@gmail.com>
2023-02-06 23:31:16 -0500

    basic layout of site and began collecting item data

---
+++ script.js
@@ -0,0 +1,34 @@
+const buildingItems = [
+    {
+        name: 'Wood Wall',
+        exploAmmo: 49,
+        satchel: 3,
+        c4: 1,
+        rocket: 2,
+        grenade: 59
+    },
+    {
+        name: 'Stone Wall',
+        exploAmmo: 185,
+        satchel: 10,
+        c4: 2,
+        rocket: 4,
+        grenade: 182
+    },
+    {
+        name: 'Metal Wall',
+        exploAmmo: 400,
+        satchel: 23,
+        c4: 4,
+        rocket: 8,
+        grenade: 993
+    },
+    {
+        name: 'Wood Wall',
+        exploAmmo: 49,
+        satchel: 3,
+        c4: 1,
+        rocket: 2,
+        grenade: 59
+    },
+]
