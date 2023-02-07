ba71cb09410b8d6b3812133734862246080b106f
Koby Moon <moonkoby1@gmail.com>
2023-02-07 13:06:19 -0500

    Added images to project

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const buildingItems = [
         satchel: 3,
         c4: 1,
         rocket: 2,
-        grenade: 59
+        image: 'wooden-wall.jpg'
     },
     {
         name: 'Stone Wall',
@@ -13,7 +13,7 @@ const buildingItems = [
         satchel: 10,
         c4: 2,
         rocket: 4,
-        grenade: 182
+        image: 'stone-wall.jpg'
     },
     {
         name: 'Metal Wall',
@@ -21,14 +21,65 @@ const buildingItems = [
         satchel: 23,
         c4: 4,
         rocket: 8,
-        grenade: 993
+        image: 'metal-wall.jpg'
     },
     {
-        name: 'Wood Wall',
-        exploAmmo: 49,
-        satchel: 3,
+        name: 'Armored Wall',
+        exploAmmo: 799,
+        satchel: 46,
+        c4: 8,
+        rocket: 15,
+        image: 'armored-wall.jpg'
+    },
+    {
+        name: 'Wooden Door',
+        exploAmmo: 19,
+        satchel: 2,
+        c4: 1,
+        rocket: 1,
+        image: 'wood-door.jpg'
+    },
+    {
+        name: 'Sheet Metal Door',
+        exploAmmo: 63,
+        satchel: 4,
         c4: 1,
         rocket: 2,
-        grenade: 59
+        image: 'metal-door.jpg'
+    },
+    {
+        name: 'Armored Door',
+        exploAmmo: 250,
+        satchel: 15,
+        c4: 3,
+        rocket: 5,
+        image: 'armored-door.png'
+    }
+]
+
+const craftingExplosiveCost = [
+    {
+        name: 'Explosive Ammo',
+        sulfurCost: 50,
+        metalCost: 10,
+        charcoalCost: 60
+    },
+    {
+        name: 'Satchel',
+        sulfurCost: 480,
+        metalCost: 80,
+        charcoalCost: 720
+    },
+    {
+        name: 'C4',
+        sulfurCost: 2200,
+        metalCost: 200,
+        charcoalCost: 3000
+    },
+    {
+        name: 'Rocket',
+        sulfurCost: 50,
+        metalCost: 10,
+        charcoalCost: 60
     },
 ]
